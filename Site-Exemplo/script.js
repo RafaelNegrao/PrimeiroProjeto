093ee4a15735fa5ae73506b854ec6fffa53b82d1
Rafael Negrão de Souza <rafaelneg2015@gmail.com>
2021-10-18 17:47:33 -0300

    Alteração feita apenas na pasta script!

--- Site-Exemplo/script.js
+++ Site-Exemplo/script.js
@@ -1,1 +1,10 @@
 alert("Está mensagem está referenciada de um script externo do arquivo index")
+
+var button = document.querySelector('button')
+
+button.onclick = function(){
+    var nome = prompt("Qual seu nome?")
+    alert('Olá ' + nome +','+'obrigado por conhecer meu site!')
+
+
+}
