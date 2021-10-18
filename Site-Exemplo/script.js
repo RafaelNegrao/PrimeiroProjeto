6e069246135168ffb9821be5afe7da1176ac4e14
Rafael Negrão de Souza <rafaelneg2015@gmail.com>
2021-10-18 18:29:20 -0300

    Caso o valor for > que mil,será cobrado um juros simples de 10%

--- Site-Exemplo/script.js
+++ Site-Exemplo/script.js
@@ -5,6 +5,19 @@ var button = document.querySelector('button')
 button.onclick = function(){
     var nome = prompt("Qual seu nome?")
     alert('Olá ' + nome +','+'obrigado por conhecer meu site!')
+    var preco = prompt("Qual o preço do item que deseja comprar?")
+    var parcelas = prompt("Qual o prazo de pagamento?")
+       
+        if (preco > 1000){
+          ValorSemJuros =(preco/parcelas) 
+          ValorPrestacao=ValorSemJuros+(ValorSemJuros * 0.10)
+        }
+        else{
+          ValorPrestacao =(preco/parcelas)
+        }
+
+        alert ("Você irá pagar "+parcelas +" parcelas no valor de "+ ValorPrestacao +" reais")
+
 
 
 }
